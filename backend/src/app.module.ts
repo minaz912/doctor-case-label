c541fe9de51fe6b2fa075de720d5388e6bffe6e1
Mina Beshay <minaz922@gmail.com>
2021-02-09 08:56:43 +0200

    Set up config loading from env file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+      cache: true,
+    }),
     MongooseModule.forRoot('mongodb://localhost:27017doctor-case-label'),
   ],
   controllers: [AppController],
