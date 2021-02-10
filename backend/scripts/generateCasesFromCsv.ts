c0116bf134767a90e9306cc0e23e0523f209efab
Mina Beshay <minaz922@gmail.com>
2021-02-10 10:37:56 +0200

    Create simple script to convert cases from CSV to JSON

---
+++ backend/scripts/generateCasesFromCsv.ts
@@ -0,0 +1,32 @@
+import * as parse from 'csv-parse';
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+(() => {
+  const inputFilePath = join(__dirname, 'conditions.csv');
+  const outputFilePath = join(__dirname, 'conditions.json');
+  const data = readFileSync(inputFilePath);
+
+  const output = [];
+
+  const parser = parse(data, {
+    delimiter: '\t',
+    columns: ['code', 'description'],
+  });
+
+  // Use the readable stream api
+  parser.on('readable', () => {
+    let record;
+    while ((record = parser.read())) {
+      output.push(record);
+    }
+  });
+
+  parser.on('error', (err) => {
+    console.error(err);
+  });
+
+  parser.on('end', () => {
+    writeFileSync(outputFilePath, JSON.stringify(output, null, 2));
+  });
+})();
