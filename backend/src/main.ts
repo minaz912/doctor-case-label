c4e0a74d65aaa2254e2cff1184235a34b7ec34a8
Mina Beshay <minaz922@gmail.com>
2021-02-13 20:33:42 +0200

    Set up openapi spec generation and ui for endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Case Labeling')
+    .setDescription('Simple API to label cases by ICD-10 condition codes')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const configService: ConfigService = app.get(ConfigService);
 
