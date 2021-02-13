f737b8506199001d323ad5076bd29fa7cc3882fe
Mina Beshay <minaz922@gmail.com>
2021-02-14 01:42:30 +0200

    Create implementation for findById

--- backend/src/cases/cases.service.ts
+++ backend/src/cases/cases.service.ts
@@ -8,7 +8,14 @@ export class CasesService {
   constructor(@InjectModel(Case.name) private caseModel: Model<CaseDocument>) {}
 
   /**
-   * Returns the next unlabeled case by creation time
+   * Finds and returns a case by id, null otherwise
+   */
+  async getById(caseId: Types.ObjectId): Promise<CaseDocument | null> {
+    return this.caseModel.findById(caseId);
+  }
+
+  /**
+   * Finds and returns the next unlabeled case by creation time
    * (cases created earlier are returned first), nor null
    * if there are no unlabeled cases
    */
