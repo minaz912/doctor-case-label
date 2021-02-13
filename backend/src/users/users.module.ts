fdda535f277eb23e88eeb6eb488bab8e66b55606
Mina Beshay <minaz922@gmail.com>
2021-02-13 17:58:53 +0200

    Add service implementation for create user

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  providers: [UsersService],
+})
 export class UsersModule {}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(input: Partial<User>): Promise<UserDocument> {
+    // TODO: hash/salt the password input before saving
+    return this.userModel.create(input);
+  }
+}
