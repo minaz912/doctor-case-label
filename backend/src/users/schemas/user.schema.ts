c6ad53d1378d3b3c4e6776e4a3c62590812d4a3a
Mina Beshay <minaz922@gmail.com>
2021-02-13 17:57:05 +0200

    Minor schema tweaks

--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class User {
@@ -8,11 +9,13 @@ export class User {
   @Prop({ type: String, required: true })
   lastName: string;
 
-  @Prop({ type: String, required: true })
+  @Prop({ type: String, required: true, index: true, unique: true })
   email: string;
 
   @Prop({ type: String, required: true })
   password: string;
 }
+
+export type UserDocument = User & Document;
 
 export const UserSchema = SchemaFactory.createForClass(User);
