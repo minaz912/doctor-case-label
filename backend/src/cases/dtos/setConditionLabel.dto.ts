fc322a56b4f5f1bdec279183fbb086d8e5f932d7
Mina Beshay <minaz922@gmail.com>
2021-02-14 01:39:31 +0200

    Rename file for consistency

--- backend/src/cases/dtos/setConditionLabel.dto.ts
+++ backend/src/cases/dtos/setConditionLabel.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 import { IsValidICD10ConditionCode } from '../validators/isValidICD10ConditionCode.validator';
+
 export class SetConditionLabelDto {
   @IsString()
   @IsNotEmpty()
   @IsValidICD10ConditionCode()
+  @ApiProperty()
   code: string;
 }
