04c001f2fd69ca6e4cea34bbaa1ff9edc1d54393
Mina Beshay <minaz922@gmail.com>
2021-02-15 01:26:12 +0200

    Add a couple of test cases

--- backend/src/cases/cases.service.spec.ts
+++ backend/src/cases/cases.service.spec.ts
@@ -1,12 +1,36 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
 import { CasesService } from './cases.service';
 
 describe('CasesService', () => {
   let service: CasesService;
 
+  class CaseModel {
+    static create = jest.fn().mockImplementation((input) => input);
+    static findOne = jest.fn().mockImplementation(() => {
+      return {
+        _id: Types.ObjectId(),
+        description: 'Example',
+      };
+    });
+    static findById = jest.fn().mockImplementation((_id) => {
+      return {
+        _id: Types.ObjectId(_id),
+        description: 'Example',
+      };
+    });
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CasesService],
+      providers: [
+        {
+          provide: getModelToken('Case'),
+          useValue: CaseModel,
+        },
+        CasesService,
+      ],
     }).compile();
 
     service = module.get<CasesService>(CasesService);
@@ -14,5 +38,23 @@ describe('CasesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a case based on input', async () => {
+      const input = { description: 'Example' };
+      const createdCase = await service.create(input);
+      expect(CaseModel.create).toHaveBeenCalledWith(input);
+      expect(createdCase).toEqual(input);
+    });
+  });
+
+  describe('getById', () => {
+    it('should return case based on id', async () => {
+      const caseId = Types.ObjectId();
+      const caseDoc = await service.getById(caseId);
+      expect(CaseModel.findById).toHaveBeenCalledWith(caseId);
+      expect(String(caseDoc._id)).toEqual(String(caseId));
+    });
   });
 });
