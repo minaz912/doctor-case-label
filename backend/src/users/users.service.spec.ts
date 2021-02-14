ca7755f112bcb2c5716ee24ba54c1680299f51a8
Mina Beshay <minaz922@gmail.com>
2021-02-14 04:04:00 +0200

    Add test cases for users service

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,12 +1,36 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const users = [
+    {
+      email: 'john_doe@example.com',
+    },
+    {
+      email: 'jane_doe@example.com',
+    },
+  ];
+  class UserModel {
+    static create = jest.fn().mockImplementation((input) => input);
+    static findOne = jest
+      .fn()
+      .mockImplementation(({ email }) =>
+        users.find((user) => user.email === email),
+      );
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        {
+          provide: getModelToken('User'),
+          useValue: UserModel,
+        },
+        UsersService,
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +38,26 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('[findByEmail]::should do a find operation with correct args', async () => {
+    const email = 'john_doe@example.com';
+    const user = await service.findByEmail(email);
+    expect(UserModel.findOne).toHaveBeenCalledWith({
+      email,
+    });
+    expect(user).toBeDefined();
+  });
+
+  it('[create]::should pass input to model', async () => {
+    const input = {
+      email: 'jane_doe@example.com',
+      firstName: 'Jane',
+      lastName: 'Doe',
+      password: 'test',
+    };
+    const created = await service.create(input);
+    expect(UserModel.create).toHaveBeenCalledWith(input);
+    expect(created).toEqual(input);
   });
 });
