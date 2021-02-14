12fa59b2c5998e83077323b61ab20d44eaf1ad6c
Mina Beshay <minaz922@gmail.com>
2021-02-14 02:55:27 +0200

    Implement findByEmail in UsersService

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -7,6 +7,10 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+  async findByEmail(email: string): Promise<UserDocument | null> {
+    return this.userModel.findOne({ email });
+  }
+
   async create(input: Partial<User>): Promise<UserDocument> {
     // TODO: hash/salt the password input before saving
     return this.userModel.create(input);
