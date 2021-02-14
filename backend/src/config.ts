f84d1459de48ccac3758e67b8660b4d91b7a5290
Mina Beshay <minaz922@gmail.com>
2021-02-14 15:41:05 +0200

    Change default port

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   env: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10) || 4000,
   mongodbUri:
     process.env.MONGODB_URI || 'mongodb://localhost:27017/doctor_case_label',
 });
